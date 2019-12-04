38c0ff6480cbde6b643bc40c9431d67f6482fdca
Eric Bryant <eric.bryant0@gmail.com>
2019-12-03 19:13:49 -0500

    pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/ssbu-character-select/' : '/'
+    process.env.NODE_ENV === 'production' ? '/ssbu-character-select/' : '/',
+  pwa: {
+    name: 'SSBU Character Select Screen',
+    themeColor: '#FF3737',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+  }
 }
