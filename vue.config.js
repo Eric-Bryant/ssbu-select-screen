4adcdb69e707036cc5f4a7a366e605ff2683c1a0
Eric Bryant <eric.bryant0@gmail.com>
2019-12-04 19:03:30 -0500

    pwa config changes

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   publicPath:
-    // process.env.NODE_ENV === 'production' ? '/ssbu-character-select/' : '/',
-    '/',
+    process.env.NODE_ENV === 'production' ? '/ssbu-character-select/' : '/',
   pwa: {
     name: 'SSBU Select',
     themeColor: '#FF3737',
